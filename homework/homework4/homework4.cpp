88ec0ca25622e05a948c22a1c6c17319287b09a5
ilya4542 <ilya3842445@gmail.com>
2021-02-09 12:44:23 +0400

    Lesson 4 homework4.cpp

---
+++ homework/homework4/homework4.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+//lesson4 task1
+  int a;
+  int b;
+
+  cout << "Введите число a ";
+  cin >> a;
+  cout << "Введите число b ";
+  cin >> b;
+	
+	
+  if (10 <= a+b && a+b <= 20) 
+    {
+      cout << "true" << endl;
+    } 
+  else 
+    {
+      cout << "false" << endl;
+    }
+  cout << "summa= " << a + b << endl;
+	
+
+//lesson 4 task 2 
+
+  /*float a=0.000;
+    int b=0;
+    float c=0.000;
+    int d=1; 
+
+    cout << "Введите число ";
+    cin >> a;
+		
+    if (a <=3)
+    { 
+    cout << "Число " << a << " натуральное простое" << endl;
+    return a;
+    } else do
+    {
+    d++;
+    b = a/d;
+    c = a/d;
+		
+    cout << "B= " << b << endl;
+    cout << "C= " << c << endl;
+    cout << "D= " << d << endl;
+    } while (c-b != 0 || c != b);
+    if (a != d && c == b)
+    {
+    cout << "Число " << a << " НЕ натуральное простое" << endl;
+    } else 
+    { 
+    cout << "Число " << a << " натуральное простое" << endl;
+    }*/ 
+
+		
+				
+
+
+//lesson4 task3
+  /*int a=7;
+    int b=4;
+
+    if (a==10)
+    {
+    cout << "true a= " << a << endl;
+
+    } else if (b==10) 
+    {
+
+    cout << "true b= " << b <<endl;
+    } else if (a+b == 10)
+    {
+    cout << "true a+b= " << a+b << endl;
+    } else 
+    {
+    cout << "false" << endl;
+    }*/
+
+  return 0;
+}
